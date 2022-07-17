8dfcc57706c46892f6bc4cee247c0fb7b3f9dc90
Ludmylla <ludmay4@gmail.com>
2022-07-17 15:46:33 -0300

    Projeto iniciado

---
+++ src/main/java/com/github/apirestjunit/ApiRestJunitApplication.java
@@ -0,0 +1,13 @@
+package com.github.apirestjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestJunitApplication.class, args);
+	}
+
+}
